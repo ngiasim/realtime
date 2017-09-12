f5e2dc5a33352c27104b26248c6a914b723d90cd
ngiasim <31400312+ngiasim@users.noreply.github.com>
2017-09-11 23:00:35 -0700

    Seeds Added
    Seeds Added

---
+++ src/seeds/DemoSeeder.php
@@ -0,0 +1,16 @@
+<?php
+namespace Websanova\Demo\Seeds;
+use Illuminate\Database\Seeder;
+class DemoSeeder extends Seeder
+{
+    public function run()
+    {
+        \DB::table('websanova_demo_items')->insert([
+        	'slug' => 'test',
+        	'name' => 'Test',
+        	'description' => 'My first item test.',
+        	'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+    }
+}
